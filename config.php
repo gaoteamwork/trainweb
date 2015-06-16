5c7c6926831c6709a82063f53aee8b4f764e9e27
sribonrean <gumafearspu@gmail.com>
2015-06-16 15:59:35 +0700

    php

--- config.php
+++ config.php
@@ -1,11 +1,17 @@
 <?php
-2.
-$host = "localhost";
-3.
-$username = "root";
-4.
-$password = "root";
-5.
-mysql_connect($host,$username,$password);
-6.
-?>
+    
+	$HOST="localhost"; 
+  	$DB_USER="thbrooms_admin"; 
+ 	$DB_PWD="admin882"; 
+ 	$DB_NAME="thbrooms_butierooms";
+  
+  $DB_HOST=(!empty($PORT)) ? $HOST.":".$PORT : $HOST;
+  
+  if(@mysql_connect($DB_HOST,$DB_USER,$DB_PWD)){
+	  $conServ=@mysql_select_db($DB_NAME) or die("SQL Error: <br>".mysql_error());	
+	  mysql_query("SET NAMES UTF8"); 
+  }else{
+	  die("SQL Error: <br>".mysql_error());	  
+  }
+  
+  ?>
