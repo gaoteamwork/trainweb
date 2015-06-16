a885941c1f79a2ec9ecf5dd2c86c22e349317247
sribonrean <gumafearspu@gmail.com>
2015-06-16 14:59:08 +0700

    แก้11

---
+++ CategoryAdmin.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html >
+  <head>
+   		
+   		 <title>ADMIN SMART ICT</title>
+    	<meta name="description" content="Flat UI Kit Free is a Twitter Bootstrap Framework design and Theme, this responsive framework includes a PSD and HTML version."/>
+
+   		 <meta charset="UTF-8" name="viewport" content="width=1000, initial-scale=1.0, maximum-scale=1.0">
+
+   		 <!-- Loading Bootstrap -->
+   		 <link href="dist/css/vendor/bootstrap.min.css" rel="stylesheet">
+
+  		  <!-- Loading Flat UI -->
+  		  <link href="dist/css/flat-ui.css" rel="stylesheet">
+  		  <link href="docs/assets/css/demo.css" rel="stylesheet">
+
+   		 <link rel="shortcut icon" href="img/favicon.ico">
+
+    
+  </head>
+  <body>
+  
+ 			<div class="container ">
+	  			<div class="row ">
+				
+					 <div class="col-sm-12 col-xs-12 ">
+						<nav class=" thumbnail ">
+							<div class="" align = "center" ><h1 class="text-info">Category</h1></div>
+					 	</nav>
+		 			 </div> 
+	 			 </div>
+ 			 </div>
+ 			
+ 			 
+  	 	<div class="container">
+  	   	<div class="row" >
+  	   	<div class="col-sm-12 col-md-12 col-xs-12">
+  	   	<nav class="thumbnail  ">
+			<table class="table table-hover  table-bordered" >
+			<tr>
+				<caption><h3>ตารางประเภทสินค้า</h3></caption>
+				
+					<Th>No.ID</Th>
+					<Th>Name</Th>
+					<Th>Type</Th>
+				
+			</tr>
+			
+			<tr>
+					<td>1</td>
+					<td>WAN</td>
+					<td>01</td>
+					
+			</tr>
+			
+			<tr>
+					<td>2</td>
+					<td>LAN</td>
+					<td>02</td>
+				
+			</tr>
+			
+			
+			
+			
+	        </table>
+  	   	</nav>
+  	   	</div>
+  	   	</div>
+  	   		
+  	   	</div>
+  	  	 	<div class="container" >
+  	  			<div class="row" >
+		  	   	<div class="col-xs-3 col-sm-4  col-md-3">
+		       	  <a href="#" class="btn  btn-lg btn-warning">Edit</a>
+		        </div>
+		     
+		     	<div class="col-xs-3 col-sm-4  col-md-3">
+		           <a href="#" class="btn  btn-lg btn-success">Save</a>
+		        </div>
+		     	
+		        <div class="col-xs-3 col-sm-4  col-md-3">
+		       	   <a href="#" class="btn  btn-lg btn-danger">Delete</a>
+		        </div>
+		   		</div>
+		   		
+    		 </div>
+		
+			<br>
+			<div class="container" >
+				   <div class="col-xs-6 col-sm-6  col-md-6">
+				         <div class="form-group has-success">
+				            <input type="text" value=""Success"" placeholder="Input ID" class="form-control" />
+				         </div>
+				   </div>
+				   
+				    <div class="col-xs-6 col-sm-6  col-md-6">
+				         <div class="form-group has-success">
+				            <input type="text" value=""Success"" placeholder="Input Name" class="form-control" />
+				         </div>
+				   </div>
+				   
+				   <div class="col-xs-6 col-sm-6  col-md-6">
+				         <div class="form-group has-success">
+				            <input type="text" value=""Success"" placeholder="Input Type" class="form-control" />
+				         </div>
+				   </div>	
+			</div>	
+			
+			<div class="container" >
+					<div class="col-xs-3 col-sm-4  col-md-3">
+				           <a href="#" class="btn  btn-lg btn-info">Add</a>
+				    </div>	      
+  	   		
+  	   		
+				
+					<br>
+					<br>
+					<br>
+  	   	
+   			</div>
+     		 
+   
+  </body>
+</html>
