ba16fbf38117510e7dfb389d5acf9e13e83da712
prapan <prapan.udo@spumail.net>
2015-06-19 14:24:11 +0700

    เพิ่ม ข้อมูลใน service content

--- service_cat_connect_fiber_optic.php
+++ service_cat_connect_fiber_optic.php
@@ -36,16 +36,59 @@
 				</div>
 			</div>
 		</nav>
-	<div class="panel panel-primary" >...</div>
+	<div class="panel panel-info" >...</div>
 	<div class="panel panel"></div>
-	<h1 class="text-center text-primary">Cat Connect Fiber Optic</h1>
+	<br/>
+
+	
+	
+	<div class="container">
+	<div class="row">
+		<nav class="panel panel-primary" >
+			<div align ="center" ><h1 class="text-info">CAT Connect Fiber Optic</h1></div>
+			<div align ="" ><h2 class="text-info">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fiber Optical Link</h2></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;บริการสำหรับลูกค้าองค์กรเพื่อการใช้บริการ Internet ความเร็วสูง ราคาประหยัด 
+			Fiber Optic Link เป็นบริการอินเทอร์เน็ตความเร็วสูงที่ให้บริการผ่านข่ายสายใยแก้วนำแสงที่มีประสิทธิภาพสูงในการรับ-ส่งข้อมูลและมีการสูญเสียของสัญญาณน้อยมาก จึงทำให้สามารถรับ–ส่งได้ระยะทางไกล และได้ความเร็วที่สูงกว่าเทคโนโลยี
+			xDSL มาก ด้วยความสามารถที่โดดเด่นจึงทำให้ บริการ Fiber Optic Link สามารถให้บริการแบนด์วิดท์ที่สูงขึ้น และบริการเสริมต่าง ๆ ไม่ว่าจะเป็น Triple-Play เช่น ชมภาพยนตร์ออนไลน์ ฟังเพลงออนไลน์, 
+			 เล่นเกมออนไลน์, Video On Demand (VOD) , ดูทีวีออนไลน์ และการท่องโลกออนไลน์ได้อย่างรวดเร็วทันใจ</p></div>
+		</nav>
 		
-	
-	
-	
-	
-	
-			<footer class="container-fluid">
+		<div class="panel panel"></div>
+		<div class="panel panel"></div>
+				<div align="center"><img src="ServicePic/catconnect.jpg" width="55%" height="55%"></div>
+		<div class="panel panel"></div>
+		<div class="panel panel"></div>
+		
+		<nav class="panel panel-primary">
+			<div align ="center"><h2 class="text-info"> รูปแบบการให้บริการ</h2></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Fiber Optic Link จะให้บริการผ่านอุปกรณ์ที่เรียกว่า OLT (Optical Line Terminal ) 
+				ซึ่งทำหน้าที่เป็นชุมสายไฟเบอร์ ออปติก จากนั้นจะส่งผ่านสายใยแก้วนำแสงและผ่านอุปกรณ์ Optical Splitter เพื่อแบ่งสายใยแก้วนำแสง (Fiber Optic) หนึ่งเส้นจากชุมสายไฟเบอร์ออปติกออกเป็นหลาย ๆ เส้นเพื่อไปยังปลายทาง
+				 แล้วต่อเข้ากับอุปกรณ์ ONU : Optical Network Unit (สำหรับ GE-PON) ไม่ว่าจะเป็นที่พักอาศัย หมู่บ้านจัดสรร อาคารสำนักงาน โดย CAT จะติดตั้งคู่สายแบบใยแก้วนำแสงไปยังผู้ใช้บริการ</p></div>
+		</nav>
+		<nav class="panel panel-primary" >
+			<div align ="center" ><h2 class="text-info">อัตราค่าบริการ</h2></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fiber Optic Link มีแพ็คเกจสำหรับให้บริการ 3 แพ็คเกจ ดังนี้ Silver , 
+			Gold และ Platinum ซึ่งแพ็คเกจต่าง ๆ เหมาะสำหรับกลุ่มบุคคลทั่วไป กลุ่มลูกค้า SME จนถึงกลุ่ม Corporate</p></div>
+		</nav>
+	<div class="panel panel"></div>
+	<div class="panel panel"></div>
+			<div align="center"><img src="ServicePic/catconnect2.jpg" width="55%" height="55%"></div>
+	<div class="panel panel"></div>
+	<div class="panel panel"></div>
+
+
+		
+
+		
+		
+		
+		
+		</div>
+	</div>
+		
+		
+			
+		<footer class="container-fluid">
 				<div class="alert alert-info">
 				<div class="row">
 					<div class="col-sm-6 col-xs-6">
@@ -60,10 +103,6 @@
 				</div>
 				</div>
 			</footer>
-	
-	
-	
-			
 			
 			
 	<script src="js/jquery-1.10.2.js"></script>
--- service_pbx.php
+++ service_pbx.php
@@ -38,14 +38,70 @@
 		</nav>
 	<div class="panel panel-info" >...</div>
 	<div class="panel panel"></div>
-	<h1 class="text-center text-primary">PBX Link Trunk</h1>
 	<br/>
-	<br/>
-	<br/>
+
 	
 		
-	
-			
+	<div class="container">
+		<nav class="panel panel-primary" >
+			<div align ="center" ><h1 class="text-info">PBX Link Trunk</h1></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;บริการด้านการสื่อสารที่ออกแบบมาเพื่อช่วยให้ต้นทุนหรือค่าใช้จ่ายขององค์กรลดลงได้มากถึง 30-40%</h3>
+		<p>เป็นการสื่อสารผ่านระบบเครือข่ายอินเตอร์เน็ตความเร็วสูงที่ออกแบบให้สามารถนำมาประยุกต์ใช้กับโทรศัพท์พื้น
+			ฐาน ในสำนักงานเพื่อโทรออกไปยังโทรศัพท์พื้นฐานด้วยกันหรือโทรศัพท์มือถือทุกเครือข่ายในอัตราประหยัด
+			 รวมถึงสามารถโทรทางไกลภายในประเทศและต่างประเทศด้วยราคาค่าโทรที่ต่ำกว่า ช่วยลดต้นทุนขององค์กรได้มากขึ้น</p></div>
+		</nav>
+		<nav class="panel panel-primary">
+			<div align ="center"><h2 class="text-info">คุณสมบัติพิเศษของ SMART ICT PBX Link</h2></div>
+			<div><h4 class="text-info">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Domestic Calls โทรศัพท์ภายในประเทศ จุดเด่นและข้อดี</h4></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ลดค่าใช้จ่ายขององค์กรในการโทรภายในประเทศ 
+			ที่หมายเลขโทรศัพท์พื้นฐานทั้งกรุงเทพฯและต่างจังหวัด
+			 ด้วยอัตราค่าบริการค่าโทรต่ำกว่า 1 บาทต่อนาที อัตราเดียวกันทั่วประเทศ
+			ลดค่าใช้จ่ายสำหรับการโทรศัพท์ไปยังมือถือ ในอัตราค่าโทรตํ่ากว่า 1 บาทต่อนาที ทุกเครือข่ายทั่วประเทศโทรฟรีระหว่างสำนักงานหรือสาขาในเครือข่ายได้ไม่จำกัด
+			คุณภาพเสียงดี และเป็นบริการที่ยืดหยุ่นโดยออกแบบให้เหมาะสมกับประเภทของธุรกิจของคุณ</p></div>
+		</nav>
+		<nav class="panel panel-primary" >
+			<div align ="center" ><h2 class="text-info">International Calls โทรศัพท์ทางไกลต่างประเทศ จุดเด่นและข้อดี</h2></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ช่วยลดค่าใช้จ่ายขององค์กร สำหรับการโทรศัพท์ทางไกลไปต่างประเทศ
+			สามารถโทรต่างประเทศ ทั้งหมายเลขพื้นฐานและมือถือ ในอัตราค่าโทรที่ประหยัด
+			อัตราค่าโทรต่างประเทศเริ่มต้นที่ 1 บาทต่อนาที
+			คุณภาพเสียงดี สามารถโทรทางไกลต่างประเทศได้กว่า 200 ประเทศปลายทาง ทั่วโลก</p></div>
+		</nav>
+	<div class="panel panel"></div>
+	<div class="panel panel"></div>
+			<img src="ServicePic/pbx.png" width="100%" height="100%">
+	<div class="panel panel"></div>
+	<div class="panel panel"></div>
+
+
+		<nav class="panel panel-primary" >
+			<div align ="center" ><h2 class="text-info">SMART ICT PBX Link services</h2></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;โทรภายในประเทศนาทีตํ่ากว่า 1 บาท ทุกเครือข่าย (ตํ่าสุดถึง 0.90 บาท/นาที)
+			โทรต่างประเทศกว่า 200 ประเทศ เริ่มต้นนาทีละ 1 บาท (ขึ้นอยู่กับเงื่อนไขและรายการส่งเสริมการขายในขณะนั้น)
+			ให้บริการโทรพร้อมกัน 8-16 Calls Concurrent
+			สัญญา 1 ปี</p></div>
+			<div align ="left" ><h3 class="text-info">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;สำหรับโทรในประเทศ</h3></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;แบบ คิดตามจริงคิด 90 ส.ต./นาที ทุกเคลื่อข่าย อัตราค่าโทรพิเศษสำหรับ
+				การโทรออกไปยังเลขหมายปลายทางภายในเครือข่ายมือถือ (เอไอเอส จีเอสเอ็ม,
+				 จีเอสเอ็ม 1800 และเอไอเอส วัน-ทู-คอล, DTAC ,TRUE ) รวมถึง
+				  ต่างจัดหวัด สำหรับ การโทรออกหาโทรพื้นฐาน (Fix Line) 02 คิดครั้งละ
+				 2.50 บาท ไม่จำกัดเวลา
+				แบบการจัดทำระบบให้ สาขาโทรหากัน ฟรี คิดค่าบริการจากการคำนวน ยอด 30 % 
+				ของค่าใช้จ่ายเดิมซึ่ง เป็นค่าดูแลระบบ</p></div>
+		
+		
+			<div align ="left" ><h3 class="text-info">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;สำหรับโทรต่างประเทศ</h3></div>
+			<div class="p"><p class="big">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;อัตราค่าโทรเริ่มต้นที่ 1 บาท/นาที (ขึ้นอยู่กับเงื่อนไขและรายการ
+			ส่งเสริมการขายในขณะนั้น) สามารถโทรปลายทางต่างประเทศกว่า 200 ประเทศทั่วโลก</p></div>
+		</nav>
+
+		
+		
+		
+		
+		
+	</div>
+		
+		
 			
 			
 			
