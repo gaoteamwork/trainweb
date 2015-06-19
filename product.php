8dc5cdc3115c95502c11fcd3e345ec2be7bbb2e0
prapan <prapan.udo@spumail.net>
2015-06-19 12:01:56 +0700

    เพิ่มservice

--- product.php
+++ product.php
@@ -47,21 +47,21 @@
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11wireless.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">Wireless</a></h3>
+		        <h3><a href ="product_wireless.php" class="text-success">Wireless</a></h3>
 		      </div>
 		  </div>
 		
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11Switches.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">Switches</a></h3>
+		        <h3><a href ="product_switches.php" class="text-success">Switches</a></h3>
 		      </div>
 		  </div>
 		
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11img.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">iMG</a></h3>
+		        <h3><a href ="product_img.php" class="text-success">iMG</a></h3>
 		      </div>
 		  </div>
 		</div>
@@ -70,21 +70,21 @@
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11imap.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">iMAP</a></h3>
+		        <h3><a href ="product_imap.php" class="text-success">iMAP</a></h3>
 		      </div>
 		  </div>
 		
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11network interface cards.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">Network Interface Cards</a></h3>
+		        <h3><a href ="product_network_interface_cards.php" class="text-success">Network Interface Cards</a></h3>
 		      </div>
 		  </div>
 		
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11security appliances.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">Security Appliances</a></h3>
+		        <h3><a href ="product_security_appliances .php" class="text-success">Security Appliances</a></h3>
 		      </div>
 		  </div>
 		</div>
@@ -93,21 +93,21 @@
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11media converters.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">Media Converters</a></h3>
+		        <h3><a href ="product_media_converters.php" class="text-success">Media Converters</a></h3>
 		      </div>
 		  </div>
 		
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11feel-top.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">Feel-Top</a></h3>
+		        <h3><a href ="product_feel_top.php" class="text-success">Feel-Top</a></h3>
 		      </div>
 		  </div>
 		
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11pbx solution.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">PBX Solutions</a></h3>
+		        <h3><a href ="product_pbx_solutions.php" class="text-success">PBX Solutions</a></h3>
 		      </div>
 		  </div>
 		</div>
@@ -115,7 +115,7 @@
 		  <div class="col-xs-12 col-md-4">
 		      <img src="ProductPic/Product_Pic/11Accessories.jpg" width="242" height="200" alt="...">
 		      <div class="caption">
-		        <h3><a href ="#" class="text-success">Accessories</a></h3>
+		        <h3><a href ="product_accessories.php" class="text-success">Accessories</a></h3>
 		      </div>
 		  </div>
 		</div>
