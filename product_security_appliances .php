8b2d60f1fa6c17911fa9c93131ab21b6a99486fb
prapan <prapan.udo@spumail.net>
2015-07-02 13:18:00 +0700

    เปลี่ยน โค้ด เมนู

--- product_security_appliances .php
+++ product_security_appliances .php
@@ -13,29 +13,12 @@
 		<meta name="viewport" content="width=divice-width, initial-scale=1" charset="UTF-8">
 </head>
 <body>
-		<nav class="navbar navbar-default navbar-fixed-top">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main">
-				        <span class="sr-only">Toggle navigation</span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				    </button>
-      				<a class="navbar-brand" href="index.php"><img alt="Brand" src="Social_Icons/CompanyLogo.jpg" width="80" height="25"></a>
-				</div>
-				<div class="navbar-collapse collapse" id="navbar-collapse-main">						
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="index.php"  	class="btn btn-lg">Home</a></li>
-						<li><a href="profile.php"  	class="btn btn-lg">Profile</a></li>
-						<li><a href="product.php"  	class="btn btn-lg">Product</a></li>
-						<li><a href="activity.php"  class="btn btn-lg">Activity</a></li>
-						<li><a href="service.php"  	class="btn btn-lg">Services</a></li>
-						<li><a href="contact.php"  	class="btn btn-lg">Contact us</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+			
+		<?php
+ 	include 'Nav.php';
+		?> 
+		
+		
 		<div class="panel panel">...</div>
 		<div class="panel panel"></div>
 		<h1 class="text-center text-primary">Security Appliances</h1>
--- service_security_solution.php
+++ service_security_solution.php
@@ -55,7 +55,7 @@
 				
 	</div>
 	
-	
+	  
 	
 	
 			<footer class="container-fluid">
